db4f3845ceddff9fcb2414b29a79a8c8d4094a96
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-20 15:27:15 -0500

    Feat: started main.js file and added dishes arrays

---
+++ main.js
@@ -0,0 +1,68 @@
+// when user selects a dish open and then
+// clicks the "lets eat" button, the user
+// should see a random dish from that cat
+
+// when the dish name appears,
+// the cookpot icon disappears (.hidden)
+
+// Query Selectors here
+var radioButton = document.querySelector(".button");
+var letsCookButton = document.querySelector("#lets-cook-button");
+
+var sides = [
+  "Miso Glazed Carrots",
+  "Coleslaw",
+  "Garden Salad",
+  "Crispy Potatoes",
+  "Sweet Potato Tots",
+  "Coconut Rice",
+  "Caeser Salad",
+  "Shrimp Summer Rolls",
+  "Garlic Butter Mushrooms",
+  "Hush Puppies"
+];
+var mains = [
+  "Spaghetti and Meatballs",
+  "Pineapple Chicken",
+  "Shakshuka",
+  "Thai Yellow Curry",
+  "Bibimbap",
+  "Chicken Parmesean",
+  "Butternut Squash Soup",
+  "BBQ Chicken Burgers",
+  "Ramen",
+  "Empanadas",
+  "Chicken Fried Rice",
+  "Sheet Pan Fajitas",
+  "Margarita Pizza"
+];
+var desserts = [
+  "Apple Pie",
+  "Lemon Meringue Pie",
+  "Black Forest Cake",
+  "Banana Bread",
+  "Peach Cobbler",
+  "Cheesecake",
+  "Funfetti Cake",
+  "Baklava",
+  "Flan",
+  "Macarons",
+  "Macaroons",
+  "Chocolate Cupcakes",
+  "Pavlova",
+  "Pumpkin Pie",
+  "Key Lime Pie",
+  "Tart Tatin",
+  "Croissants",
+  "Eclairs"
+];
+var shownRecipe = [];
+
+// Event Listeners here
+letsCookButton.addEventListener("click", showRecipe)
+
+// Functions here
+function showRecipe() {
+  // show a randomly selected recipe from the given array
+  // hide the cookpit icon
+}
